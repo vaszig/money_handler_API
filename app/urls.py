77523bc723ce4f734eebd616505227733ee1d49a
lakis <zigras00@gmail.com>
2021-05-06 10:59:34 +0200

    change url for balance

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('balance/', views.get_balance, name='get_balance'),
     path('income/', views.get_sum_from_dates, {'category': 'income'}, name='get_income_sum_from_dates'),
     path('expenses/', views.get_sum_from_dates, {'category': 'expenses'}, name='get_expenses_sum_from_dates'),
     path('transactions/', views.get_filtered_transactions, name='get_filtered_transactions')
